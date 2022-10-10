b8d8b0781748a859951f07d5b070db7016b4465a
- <->
2022-10-10 22:19:57 +0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const pdfUtil = require("pdf-to-text");
+// const pdfUtil = require("pdf-to-text");
 const { PDFDocument } = require("pdf-lib");
 const fs = require("fs");
 const { getMimeType } = require("stream-mime-type");
@@ -10,65 +10,65 @@ app.use("", express.static("uploads"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const send_pdf_json = require("./utils/send_pdf_json");
+// const send_pdf_json = require("./utils/send_pdf_json");
 
 let extracted_json_data;
 let extracted_images = [];
 
-app.post("/", function (req, res, next) {
+app.post("/", async function (req, res, next) {
   extracted_images = [];
   extracted_json_data = null;
 
-  pdfUtil.pdfToText(`./uploads/doc.pdf`, async (err2, data) => {
-    const existingPdfBytes = await new Promise((resolve, reject) => {
-      fs.readFile(`./uploads/doc.pdf`, (err, result) => {
-        if (err) {
-          reject(err);
-        }
-        if (!err) {
-          resolve(result);
+  // pdfUtil.pdfToText(`./uploads/doc.pdf`, async (err2, data) => {
+  const existingPdfBytes = await new Promise((resolve, reject) => {
+    fs.readFile(`./uploads/doc.pdf`, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      if (!err) {
+        resolve(result);
+      }
+    });
+  });
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  const pages = pdfDoc.getPages();
+  const result = [];
+
+  pages[0].doc.context.indirectObjects.forEach((el) => {
+    if (el.hasOwnProperty("contents")) result.push(el.contents);
+  });
+
+  const mime = await Promise.all(
+    result.map(async (el) => {
+      return new Promise(async (resolve) => {
+        const res = await getMimeType(el);
+        if (res) {
+          resolve(res);
         }
       });
-    });
+    })
+  );
 
-    const pdfDoc = await PDFDocument.load(existingPdfBytes);
-    const pages = pdfDoc.getPages();
-    const result = [];
-
-    pages[0].doc.context.indirectObjects.forEach((el) => {
-      if (el.hasOwnProperty("contents")) result.push(el.contents);
-    });
-
-    const mime = await Promise.all(
-      result.map(async (el) => {
+  Promise.all(
+    mime.map(async (el, i) => {
+      if (el.mime === "image/jpeg") {
         return new Promise(async (resolve) => {
-          const res = await getMimeType(el);
-          if (res) {
-            resolve(res);
-          }
+          const resp = await result[i];
+          const fileContents = new Buffer.from(resp, "base64");
+          const file_name = `${Math.random().toString().slice(2)}.jpg`;
+          extracted_images.push(
+            `https://stacknodetestapi.herokuapp.com/${file_name}`
+          );
+          fs.writeFile(`uploads/${file_name}`, fileContents, (err) => {});
         });
-      })
-    );
+      }
+    })
+  );
 
-    Promise.all(
-      mime.map(async (el, i) => {
-        if (el.mime === "image/jpeg") {
-          return new Promise(async (resolve) => {
-            const resp = await result[i];
-            const fileContents = new Buffer.from(resp, "base64");
-            const file_name = `${Math.random().toString().slice(2)}.jpg`;
-            extracted_images.push(
-              `https://stacknodetestapi.herokuapp.com/${file_name}`
-            );
-            fs.writeFile(`uploads/${file_name}`, fileContents, (err) => {});
-          });
-        }
-      })
-    );
-
-    extracted_json_data = send_pdf_json(data);
-    res.send("Successfully uploaded!");
-  });
+  // extracted_json_data = send_pdf_json(data);
+  res.send("Successfully uploaded!");
+  // });
 });
 
 app.get("/get_extracted_data", (req, res) => {
