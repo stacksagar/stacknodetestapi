ac0286b82addd3620fd99efb6c4d9dfb8769672b
- <->
2022-10-10 18:50:34 +0600

    updated

--- index.js
+++ index.js
@@ -1,38 +1,101 @@
 const express = require("express");
-const multer = require("multer");
-const app = express();
 const cors = require("cors");
+const app = express();
+const multer = require("multer");
+const pdfUtil = require("pdf-to-text");
+const { PDFDocument } = require("pdf-lib");
+const fs = require("fs");
+const { getMimeType } = require("stream-mime-type");
 
-app.use("", express.static("./uploads"));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+let pdf_file_name = "";
 
-let uploaded_file_name;
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, "./uploads");
   },
-
   filename: function (req, file, callback) {
-    const new_name = Math.floor(Math.random() * 999999);
-    uploaded_file_name = new_name + "-" + file.originalname;
-    callback(null, uploaded_file_name);
+    const new_name = Math.random().toString().slice(2);
+    pdf_file_name = new_name;
+    callback(null, `${pdf_file_name}.pdf`);
   },
 });
 
-const upload = multer({ storage: storage }).single("file");
+const upload = multer({ storage: storage }).single("pdfdoc");
+
+app.use("", express.static("uploads"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const send_pdf_json = require("./utils/send_pdf_json");
+
+let extracted_json_data;
+let extracted_images = [];
 
 app.post("/", function (req, res, next) {
+  extracted_images = [];
+  extracted_json_data = null;
+
   upload(req, res, function (err1) {
-    // pdfUtil.pdfToText(`./uploads/${pdf_file_name}.pdf`, async (err2, data) => {
-    res.json({
-      message: "server is running!",
-      uploaded_file_name: `http://localhost:1000/` + uploaded_file_name,
-    });
-    // });
+    return pdfUtil.pdfToText(
+      `./uploads/${pdf_file_name}.pdf`,
+      async (err2, data) => {
+        const existingPdfBytes = await new Promise((resolve, reject) => {
+          fs.readFile(`./uploads/${pdf_file_name}.pdf`, (err, result) => {
+            if (err) {
+              reject(err);
+            }
+            if (!err) {
+              resolve(result);
+            }
+          });
+        });
+
+        const pdfDoc = await PDFDocument.load(existingPdfBytes);
+        const pages = pdfDoc.getPages();
+        const result = [];
+
+        pages[0].doc.context.indirectObjects.forEach((el) => {
+          if (el.hasOwnProperty("contents")) result.push(el.contents);
+        });
+
+        const mime = await Promise.all(
+          result.map(async (el) => {
+            return new Promise(async (resolve) => {
+              const res = await getMimeType(el);
+              if (res) {
+                resolve(res);
+              }
+            });
+          })
+        );
+
+        Promise.all(
+          mime.map(async (el, i) => {
+            if (el.mime === "image/jpeg") {
+              return new Promise(async (resolve) => {
+                const resp = await result[i];
+                const fileContents = new Buffer.from(resp, "base64");
+                const file_name = `${Math.random().toString().slice(2)}.jpg`;
+                extracted_images.push(`http://localhost:1000/${file_name}`);
+                fs.writeFile(`uploads/${file_name}`, fileContents, (err) => {});
+              });
+            }
+          })
+        );
+
+        extracted_json_data = send_pdf_json(data);
+        res.send("Successfully uploaded!");
+      }
+    );
   });
 });
 
+app.get("/get_extracted_data", (req, res) => {
+  res.json({ ...extracted_json_data, extracted_images });
+});
+
 const PORT = process.env.PORT || 1000;
-app.listen(PORT);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
